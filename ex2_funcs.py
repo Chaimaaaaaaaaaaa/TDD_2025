7a6931187544bb062369fbb79650e6df687375bf
Chaimaaaaaaaaaaa <Chaima.Ouerfili@etu.sorbonne-universite.fr>
2024-12-31 20:35:33 +0100

    add FIFO tests and prototype

---
+++ ex2_funcs.py
@@ -0,0 +1,15 @@
+class FIFO:
+    def __init__(self):
+        pass
+    
+    def add(self, item):
+        pass
+    
+    def rmv(self):
+        pass
+
+    def is_empty(self):
+        pass
+    
+    def len(self):
+        pass
---
+++ tests_ex2_funcs.py
@@ -0,0 +1,23 @@
+import ex2_funcs
+import unittest
+
+class TestEx2Funcs(unittest.TestCase):
+    # Tests FIFO
+    def tests_FIFO(self):
+        fifo = ex2_funcs.FIFO()
+        self.assertTrue(fifo.is_empty())
+        fifo.add(1)
+        fifo.add(2)
+        fifo.add(3)
+        self.assertEqual(fifo.len(), 3)
+        self.assertFalse(fifo.is_empty())
+        self.assertEqual(fifo.rmv(), 1)
+        self.assertEqual(fifo.rmv(), 2)
+        self.assertEqual(fifo.rmv(), 3)
+        
+        
+    
+    
+    
+if __name__ == '__main__':
+    unittest.main()
