733ea810ef611e0061d35393995d1dabbbc48616
Chaimaaaaaaaaaaa <Chaima.Ouerfili@etu.sorbonne-universite.fr>
2024-12-31 16:25:40 +0100

    add is_prime_number tests

--- tests_ex1_funcs.py
+++ tests_ex1_funcs.py
@@ -10,5 +10,16 @@ class TestEx1Funcs(unittest.TestCase):
         self.assertEqual(ex1_funcs.max_three_ints(-1, -2, -3), -1)
         self.assertEqual(ex1_funcs.max_three_ints(-1, 2, -3), 2)
     
+    def test_is_prime_number(self):
+        self.assertTrue(ex1_funcs.is_prime_number(2))
+        self.assertTrue(ex1_funcs.is_prime_number(3))
+        self.assertTrue(ex1_funcs.is_prime_number(5))
+        self.assertTrue(ex1_funcs.is_prime_number(7))
+        self.assertTrue(ex1_funcs.is_prime_number(11))
+        self.assertFalse(ex1_funcs.is_prime_number(1))
+        self.assertFalse(ex1_funcs.is_prime_number(4))
+        self.assertFalse(ex1_funcs.is_prime_number(6))
+        
+    
 if __name__ == '__main__':
     unittest.main()
