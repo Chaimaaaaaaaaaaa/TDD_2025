3c149a15e94269d73b9fed70c987b8d325a9c7c1
Chaimaaaaaaaaaaa <Chaima.Ouerfili@etu.sorbonne-universite.fr>
2024-12-31 19:51:33 +0100

    add is_arithmetic_sequence tests

--- tests_ex1_funcs.py
+++ tests_ex1_funcs.py
@@ -22,6 +22,17 @@ class TestEx1Funcs(unittest.TestCase):
         self.assertFalse(ex1_funcs.is_prime_number(0))
         self.assertFalse(ex1_funcs.is_prime_number(-1))
         
+    def test_is_arithmetic_sequence(self):
+        self.assertTrue(ex1_funcs.is_arithmetic_sequence([1, 2, 3, 4, 5]))
+        self.assertTrue(ex1_funcs.is_arithmetic_sequence([1, 3, 5, 7, 9]))
+        self.assertTrue(ex1_funcs.is_arithmetic_sequence([1, 1, 1, 1, 1]))  
+        self.assertTrue(ex1_funcs.is_arithmetic_sequence([1, 0, -1, -2, -3]))
+        self.assertTrue(ex1_funcs.is_arithmetic_sequence([1, -1, -3, -5, -7]))
+        self.assertFalse(ex1_funcs.is_arithmetic_sequence([1, 2, 3, 4, 6]))
+        self.assertFalse(ex1_funcs.is_arithmetic_sequence([1, 3, 5, 7, 8]))
+        self.assertFalse(ex1_funcs.is_arithmetic_sequence([1, 1, 1, 1, 2]))
+        self.assertFalse(ex1_funcs.is_arithmetic_sequence([]))
+        
     
 if __name__ == '__main__':
     unittest.main()
