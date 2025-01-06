06649d36d085490dd9ac6c5baf8edf4e354dd70f
Chaimaaaaaaaaaaa <Chaima.Ouerfili@etu.sorbonne-universite.fr>
2025-01-06 22:18:59 +0100

    ex3 add function start_record + clear names

--- ex3_capture_fablab/capture_fablab.py
+++ ex3_capture_fablab/capture_fablab.py
@@ -5,6 +5,9 @@ from datetime import datetime
 from time import sleep
 import lgpio
 
+# Chemin d'enregistrement des vidéos
+path = '/home/abc/Projet/Videos/'
+
 # Pins de la molette
 GPIO = lgpio.gpiochip_open(0)
 START_LED_PIN = 17
@@ -23,6 +26,7 @@ button_off = Button(BUTTON_OFF_PIN, bounce_time=0.2)
 # Pour sortir de la boucle d'enregistrement
 stop_enrg = True
 
+
 # Fait clignoter une LED un certain nombre de fois.
 def blink_led(pin, times, interval):
     for _ in range(times):
@@ -31,19 +35,6 @@ def blink_led(pin, times, interval):
         lgpio.gpio_write(GPIO, pin, 0)
         sleep(interval)
 
-# Interruption pour le bouton stop
-def init():
-    button_off.when_pressed = OFF
-
-# Fonction appelée par l'interruption, met la variable stop_enrg à false pour sortir de la boucle d'enregistrement
-def OFF():
-    global stop_enrg
-    stop_enrg = False
-    # Clignote 3 fois pour indiquer que l'enregistrement est terminé
-    blink_led(17, 3, 0.2)
-    # La LED verte reste s'eteint pour indiquer que l'enregistrement est terminé
-    lgpio.gpio_write(GPIO, 21, 0)
-
 # Retourne la valeur choisie par la molette + 1
 def molette():
     valeur_inv = [lgpio.gpio_read(GPIO, pin) for pin in [14, 15, 23, 24, 25]]
@@ -51,39 +42,57 @@ def molette():
     valeur_ = valeur[0] * 1 + valeur[1] * 2 + valeur[2] * 4 + valeur[3] * 8 + valeur[4] * 8 + 1
     return valeur_
 
-def main():
+
+# Interruption pour le bouton stop
+def init():
+    button_off.when_pressed = stop_record
+
+
+# Fonction appelée par l'interruption, met la variable stop_enrg à false pour sortir de la boucle d'enregistrement
+def stop_record():
     global stop_enrg
+    stop_enrg = False
+    # Clignote 3 fois pour indiquer que l'enregistrement est terminé
+    blink_led(17, 3, 0.2)
+    # La LED verte reste s'eteint pour indiquer que l'enregistrement est terminé
+    lgpio.gpio_write(GPIO, 21, 0)
+
+
+def start_record():
+    global stop_enrg
+    stop_enrg = True
+    blink_led(17, 3, 0.2) # Lancement de l'enregistrement après 3 clignotements
+    lgpio.gpio_write(GPIO, 21, 1) # La LED verte s'allume pour indiquer que l'enregistrement est en cours
+    
+    nom_fichier = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    video = cv2.VideoCapture(0)
+    if not video.isOpened():
+        print("Erreur de lecture du fichier")
+        return
+    frame_width = int(video.get(3))
+    frame_height = int(video.get(4))
+    size = (frame_width, frame_height)
+    fps = molette()
+    print("Frame rate sélectionné: ", fps)
+    result = cv2.VideoWriter(path + nom_fichier + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, size)
+    while stop_enrg: # Boucle d'enregistrement
+        ret, frame = video.read()
+        if ret:
+            result.write(frame)
+        else:
+            break
+    # Relâcher tout à la fin
+    video.release()
+    result.release()
+    print("Vidéo enregistrée\n")
+
+
+def main():
     while True:
         if button_on.is_pressed:
-            stop_enrg = True
-            # Lancement de l'enregistrement après 3 clignotements
-            blink_led(17, 3, 0.2)
-            # La LED verte s'allume pour indiquer que l'enregistrement est en cours
-            lgpio.gpio_write(GPIO, 21, 1)
-            nom_fichier = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            video = cv2.VideoCapture(0)
-            if not video.isOpened():
-                print("Erreur de lecture du fichier")
-                return
-            frame_width = int(video.get(3))
-            frame_height = int(video.get(4))
-            size = (frame_width, frame_height)
-            fps = molette()
-            print("______________________frame", fps)
-            # Modifier le chemin d'enregistrement des vidéos si necessaire
-            result = cv2.VideoWriter('/home/abc/Projet/Videos/' + nom_fichier + '.avi', cv2.VideoWriter_fourcc(*'MJPG'), fps, size)
-            while stop_enrg:
-                print("enrg")
-                ret, frame = video.read()
-                if ret:
-                    result.write(frame)
-                else:
-                    break
-            # Relâcher tout à la fin
-            video.release()
-            result.release()
-            print("Vidéo enregistrée\n")
-
+            start_record()
+            
+            
 if __name__ == "__main__":
     # Clignote 2 fois pour indiquer que le programme est lancé
     blink_led(17, 2, 0.1)
